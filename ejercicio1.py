7fa737b31f129a9d8f5b6edd70823dee124f9e47
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-09 09:52:20 +0100

    cambio

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+"""
+Ahora quedará a cargo del alumno completar la funcionalidad del TDA polinomio, dado que solo se desarrollaron algunas funciones, 
+agregándole la capacidad de eliminar términos, y de determinar si en un polinomio existe un término, para evitar tener que llamar a la función “obtener_valor” 
+y luego consultar si el resultado es distinto de cero para determinar si el polinomio tiene ese término o no. Esta última debe ser una función booleana.
+"""
+
